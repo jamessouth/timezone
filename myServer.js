2768b983159c31e2a014f05dd979c3a4d7c84697
jamessouth <jamesdanielsouth@gmail.com>
2019-05-25 12:47:22 -0500

    remove mongoose

--- myServer.js
+++ myServer.js
@@ -1,7 +1,6 @@
 const http = require('http');
 // const fs = require('fs');
 // const path = require('path');
-const mongoose = require('mongoose');
 
 const server = http.createServer((req, res) => {
 
@@ -11,26 +10,7 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, '7777777', { "Access-Control-Allow-Origin": "http://localhost:3100" });
 
 
-    mongoose.connect('mongodb://localhost/tzs', {useNewUrlParser: true});
-    const db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error: '));
-    db.once('open', () => {
-      console.log('connected!');
-      const timezoneSchema = new mongoose.Schema({
-        offset: String,
-        locations: String
-      });
 
-
-      const Timezone = mongoose.model('Timezone', timezoneSchema);
-      const utc = new Timezone({ offset: '0', locations: 'UK' });
-      console.log(utc);
-      utc.save((err, utc) => {
-        if(err) return console.error(err);
-        console.log(`${utc} added!`);
-      });
-
-    });
     res.write('done!!!', 'utf8');
 
 
