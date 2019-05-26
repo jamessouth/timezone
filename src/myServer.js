0f33d3c67af48f78b045f91a27af325b0f4913fc
jamessouth <jamesdanielsouth@gmail.com>
2019-05-26 01:39:35 -0500

    get data

--- src/myServer.js
+++ src/myServer.js
@@ -1,23 +1,43 @@
 /* eslint-disable no-console */
+import processTimezoneData from './utils/processTimezoneData';
+
+const http = require('http');
+const https = require('https');
 const MongoClient = require('mongodb').MongoClient;
+
 const client = new MongoClient('mongodb://localhost:27017', { useNewUrlParser: true });
-const server = require('http').createServer(serverCB);
-const serverCB = (req, res) => {
+const server = http.createServer(serverCB).listen(3101, () => {
+  console.log('server running on port 3101!');
+});
+
+function serverCB(req, res) {
   console.log(req.url);
   if(req.url === '/') {
     res.writeHead(200, '7777777', { 'Access-Control-Allow-Origin': 'http://localhost:3100' });
 
-    client.connect().then(() => {
-      console.log('connected successfully to mdb server!!');
-      const db = client.db('tzs');
-      client.close();
-    }).catch(err => console.error(err));
+    // processTimezoneData().then(s => console.log(s));
+    let c = 0;
+    let chunks = [];
+    https.get('https://en.wikipedia.org/w/api.php?action=parse&page=Time_zone&prop=text&section=11&format=json&origin=*', resl => {
+      resl.pipe(res);
+    	// res.on('data', chunk => {
+        // console.log(`chunk ${c++}`);
+        // console.log(JSON.parse(JSON.stringify(chunk)));
+        // console.log();
 
-    res.write('done!!!', 'utf8');
-    res.end('5');
+      // });
+    });
+
+
+
+
+    // client.connect().then(() => {
+    //   console.log('connected successfully to mdb server!!');
+    //   const db = client.db('tzs');
+    //   client.close();
+    // }).catch(err => console.error(err));
+
+    // res.write('done!!!', 'utf8');
+    // res.end();
   }
 };
-
-server.listen(3101, () => {
-  console.log('server running on port 3101!');
-});
--- src/utils/getTimezoneData.js
+++ src/utils/getTimezoneData.js
@@ -1,8 +1,12 @@
-export default async function getTimezoneData() {
+const https = require('https');
+
+export default function getTimezoneData() {
   try{
-    let response = await fetch('https://en.wikipedia.org/w/api.php?action=parse&page=Time_zone&prop=text&section=11&format=json&origin=*');
+    let response = https.get('https://en.wikipedia.org/w/api.php?action=parse&page=Time_zone&prop=text&section=11&format=json&origin=*', response => {
+
+    });
     if (!response.ok) throw new Error('Network problem - response not ok');
-    response = await response.json();
+    // response = await response.json();
     return response.parse.text['*'];
   } catch(err){
     console.error('error on fetch: ', err.message);
