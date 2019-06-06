397e64f8d89f7dcda1c749732ebf449c73d137f7
jamessouth <jamesdanielsouth@gmail.com>
2019-06-06 12:54:53 -0500

    change App component from class to function w/ hooks

--- src/App.js
+++ src/App.js
@@ -1,68 +1,63 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Form from './components/Form';
-// import getTimezoneData from './utils/getTimezoneData';
-
-// const Fragment = React.Fragment;
-export default class App extends Component{
-
-  componentDidMount() {
-    // getTimezoneData();
-  }
-// .map(x => String.fromCharcode(x)
-  sendMsg() {
-    // const evtSource = new EventSource('http://localhost:3101');
-    // evtSource.addEventListener('ping', function(e) {
-    //   console.log('p ', e);
-    // }, false);
-    // evtSource.addEventListener('error', function(e) {
-    //   console.log('err ', e);
-    // });
-
-
-    console.log(new Date());
-    fetch('http://localhost:3101')
-    // .then(x => x.json())
-    .then(async res => {
-      const readr = res.body.getReader();
-      const data = await readr.read();
-      async function processData({done, value}) {
-        if (done) return;
-        console.log(new TextDecoder('utf-8').decode(value));
-
-        return readr.read().then(processData);
-      }
-      return await processData(data);
-    });
-
-  }
 
 
 
-  async postQuery(body) {
-    try {
-      let data = await fetch('http://localhost:3101', {
-        method: 'POST',
-        body
-      });
-      if (data.ok) {
-        data = await data.json();
-        console.log(data);
-      } else {
-        throw new Error('Network problem - response not ok');
-      }
-    } catch (err) {
-      console.log(err);
+
+function sendMsg() {
+  // .map(x => String.fromCharcode(x)
+  // const evtSource = new EventSource('http://localhost:3101');
+  // evtSource.addEventListener('ping', function(e) {
+  //   console.log('p ', e);
+  // }, false);
+  // evtSource.addEventListener('error', function(e) {
+  //   console.log('err ', e);
+  // });
+
+
+  console.log(new Date());
+  fetch('http://localhost:3101')
+  // .then(x => x.json())
+  .then(async res => {
+    const readr = res.body.getReader();
+    const data = await readr.read();
+    async function processData({done, value}) {
+      if (done) return;
+      console.log(new TextDecoder('utf-8').decode(value));
+
+      return readr.read().then(processData);
     }
+    return await processData(data);
+  });
+
+}
+
+
+async function postQuery(body) {
+  try {
+    let data = await fetch('http://localhost:3101', {
+      method: 'POST',
+      body
+    });
+    if (data.ok) {
+      data = await data.json();
+      console.log(data);
+    } else {
+      throw new Error('Network problem - response not ok');
+    }
+  } catch (err) {
+    console.log(err);
   }
+}
 
 
-  render() {
+export default function App() {
+
     return (
       <>
         <div className="intro">Hello World</div>
-        <button type="button" onClick={this.sendMsg}>data</button>
-        <Form postQuery={this.postQuery}/>
+        <button type="button" onClick={sendMsg}>data</button>
+        <Form postQuery={postQuery}/>
       </>
     );
-  }
 }
