85110d570d5beda0379e420e876025ff0f4f8a8a
jamessouth <jamesdanielsouth@gmail.com>
2019-11-15 22:54:11 -0600

    gitignore exe and out files

--- src/goServer/handlers/getHandler.go
+++ src/goServer/handlers/getHandler.go
@@ -9,7 +9,6 @@ GetHandler handles a get method request sent to the server
 */
 func GetHandler(w http.ResponseWriter, r *http.Request) {
 
-	// fmt.Println("get", r)
 	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3100")
 	w.WriteHeader(http.StatusOK)
 	return
