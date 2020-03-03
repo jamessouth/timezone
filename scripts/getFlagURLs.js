dc1d521ba3977d9c1cd54ac1507771c40002e6f6
jamessouth <jamesdanielsouth@gmail.com>
2020-03-03 13:04:41 -0600

    cont base64 img conv script

--- scripts/getFlagURLs.js
+++ scripts/getFlagURLs.js
@@ -15,13 +15,17 @@ const client = new MongoClient(
 const requestAsync = function(url, file, path) {
     return new Promise((resolve, reject) => {
 
-        https.get(url, chunks => {
+        https.get(url, async chunks => {
 
-            chunks.pipe(file);
+            await chunks.pipe(file);
             // console.log('dt: ', Date.now());
+            
             fs.readFile(path, 'base64', (err, data) => {
-                if (err) return reject(err);
-                resolve(data);
+                if (err) reject(err);
+                setTimeout(() => {
+
+                    resolve(data);
+                }, 250);
             });
             
         });
@@ -35,20 +39,22 @@ async function* gengen(arr) {
     for (let i = 0; i < arr.length; i++) {
         const path = 'C:/Users/danny/Desktop/flags/' + arr[i].name + '.png';
         const file = fs.createWriteStream(path);
-        const url = 'https://' + arr[i].flag;
-        const b64 = requestAsync(url, file, path);
+        const url = arr[i].flag.startsWith('htt') ? arr[i].flag : 'https://' + arr[i].flag;
+        
+        const b64 = await requestAsync(url, file, path);
 
         // console.log('dt2: ', Date.now());
-        yield b64;
-        fs.unlinkSync(path);
+        yield Promise.all([b64, arr[i].name, arr[i].no]);
+        // fs.unlinkSync(path);
         
     }
 }
 
 async function loop(arr, db) {
     for await (const url of gengen(arr)) {
-        // console.log('res: ', url, Date.now());
-        db.collection('bu2')
+        
+        console.log('res: ', url[0].slice(40, 60), url[1], url[2], Date.now(), url[0].length);
+        // db.collection('bu2').updateOne({"places.pl":"Kiribati (Phoenix Islands)","no":36},{$set: {"places.$.fl": kir}})
     }
 }
     
@@ -65,12 +71,13 @@ async function go() {
     .toArray();
 
 
-    const urls = list.slice(0,1).flatMap(b => b.places.map(f => ({no: b.no, name: f.pl, flag: f.fl})));
+    const urls = list.slice(1,2).flatMap(b => b.places.map(f => ({no: b.no, name: f.pl, flag: f.fl})));
 
     // console.log('urls: ', urls);
 
     await client.close();
 
+    // .slice(0,1)
     loop(urls, db);
 
 }
