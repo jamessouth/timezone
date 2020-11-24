ca890f975784a35b768e2a337b3143388a601f1d
jamessouth <jamesdanielsouth@gmail.com>
2020-11-24 00:20:08 -0600

    cont mongo to dynamo

--- scripts/mongoToDynamo.js
+++ scripts/mongoToDynamo.js
@@ -11,10 +11,12 @@ const docClient = new AWS.DynamoDB.DocumentClient();
 const timezones = JSON.parse(fs.readFileSync("./tzs.json", 'utf-8'));
 let count = 0;
 let arr = [];
+let arr2 = [];
 
 
 timezones.forEach(doc => {
   arr.push(doc.offset);
+  arr2.push(doc.places);
   doc.places.forEach(pl => {
 
     const item = {
@@ -37,7 +39,46 @@ timezones.forEach(doc => {
       }
     });
   });
-
+  
   
 });
-console.log('arr: ', arr);
+const item = {
+  TableName: 'demo', 
+  Item: {
+    "offset": "offsets",
+    "place": "null",
+    "offsets": arr
+  },
+  ReturnConsumedCapacity: 'INDEXES'
+  
+};
+docClient.put(item, (err, data) => {
+  if (err) {
+    console.error("didnt add", err)
+  } else {
+    count += data.ConsumedCapacity.CapacityUnits;
+    console.log("success", count);
+  }
+});
+const item2 = {
+  TableName: 'demo',
+  Item: {
+    "offset": "places",
+    "place": "null",
+    "places": docClient.createSet([...new Set(arr2.flat().map(o => o.name))])
+  },
+  ReturnConsumedCapacity: 'INDEXES'
+  
+};
+docClient.put(item2, (err, data) => {
+  if (err) {
+    console.error("didnt add", err)
+  } else {
+    count += data.ConsumedCapacity.CapacityUnits;
+    console.log("success", count)
+  }
+});
+// console.log('arr: ', );
+
+// note : utf8 chars are ok in json but not rendered properly in dynamo
+
