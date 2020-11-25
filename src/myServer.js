5d40aad1566a0194b7f1e06297a6aeeb5ebbe7ef
jamessouth <jamesdanielsouth@gmail.com>
2020-11-25 00:42:04 -0600

    cont mongo to dynamo

--- src/myServer.js
+++ src/myServer.js
@@ -4,21 +4,24 @@ import prog from './utils/ProgressEmitter';
 import routePipe from './utils/routePipe';
 import routeMap from './utils/routeMap';
 
+const AWS = require('aws-sdk');
 const http = require('http');
 const path = require('path');
-const MongoClient = require('mongodb').MongoClient;// mongod --dbpath="c:\data\db"
+
+AWS.config.update({
+  endpoint: process.env.ENV == 'dev' ? 'http://localhost:8000' : process.env.MONGODB_URI
+});
 
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8000;
 }
 
-// eslint-disable-next-line no-unused-vars
-const server = http.createServer(serverCB).listen(port, () => {// eslint-disable-next-line no-console
+http.createServer(serverCB).listen(port, () => {// eslint-disable-next-line no-console
   console.log(`server running on port ${port}!`);// , '\x07' default beep
 });
 
-let db, client;
+let db, client, docClient;
 async function serverCB(req, res) {
   let payload, data;
   if (req.method == 'POST') {
@@ -31,7 +34,7 @@ async function serverCB(req, res) {
     req.on('end', async () => {
       const {source, type} = JSON.parse(query);
       try {
-        data = await graphql({ schema, source, contextValue: db });
+        data = await graphql({ schema, source, contextValue: docClient });
         if (data.errors) {
           throw data.errors[0];
         }
@@ -56,16 +59,12 @@ async function serverCB(req, res) {
         res.write('event: status\ndata: \n\n\n');
       });
 
-      client = new MongoClient(
-        process.env.ENV == 'dev' ? 'mongodb://localhost:27017' : process.env.MONGODB_URI,
-        {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-        }
-      );
+      docClient = new AWS.DynamoDB.DocumentClient();
+
+
 
       res.writeHead(200, {
-        // 'Access-Control-Allow-Origin': 'http://localhost:3100',
+        'Access-Control-Allow-Origin': 'http://localhost:3100',
         'Connection': 'keep-alive',
         'Content-Type': 'text/event-stream',
         'Cache-Control': 'no-cache'
@@ -74,48 +73,51 @@ async function serverCB(req, res) {
       const eventInterval = setInterval(() => res.write(':keepalive\n\n\n'), 54165);// heroku dyno times out after 55 seconds
       res.write('event: status\ndata: Connecting to database\n\n\n');
 
-      try {
-        await client.connect();
-        db = process.env.ENV == 'dev' ? client.db('tzs') : client.db(process.env.DB_NAME);
-        console.log('Connected correctly to mongo server!', !!db);// eslint-disable-line no-console
-        prog.emit('connected');
-        const records = await db
-          .collection('timezones')
-          .find({})
-          .project({ 'places.name': 1, offset: 1, _id: 0 })
-          .sort('no', 1)
-          .toArray();
-
-        const splitData = records.reduce((acc, x) => {
-          acc[0].push(...x.places.map(p => p.name));
-          acc[1].push(x.offset);
-          return acc;
-        }, [[], []]);
-
-        const pList = [...new Set(splitData[0].sort((a, b) => a.localeCompare(b)))];
-        const oList = splitData[1];
-
-        if (records.length == 0) {
-          throw new Error('Database error: Data not available');
-        } else {
-          prog.emit('datafetched', [oList, pList]);
+   
+        
+      // db = process.env.ENV == 'dev' ? client.db('tzs') : client.db(process.env.DB_NAME);
+      // console.log('Connected correctly to mongo server!', !!db);// eslint-disable-line no-console
+      prog.emit('connected');
+      const params = key => ({
+        TableName: 'demo',
+        Key: {
+          "offset": key,
+          "place": "null"
         }
+      });
+      const placesProm = docClient.get(params("places")).promise();
+      const offsetsProm = docClient.get(params("offsets")).promise();
 
-      } catch (err) {// eslint-disable-next-line no-console
-        console.log('conn err', err.message, err);
-        if (err.name == 'MongoTimeoutError') {
-          res.write(`event: status\ndata: Error connecting to database: ${err.message}. Please try again.\n\n\n`);
-        } else {
+      // const splitData = records.reduce((acc, x) => {
+      //   acc[0].push(...x.places.map(p => p.name));
+      //   acc[1].push(x.offset);
+      //   return acc;
+      // }, [[], []]);
+
+      // const pList = [...new Set(splitData[0].sort((a, b) => a.localeCompare(b)))];
+      // const oList = splitData[1];
+
+      Promise.all([placesProm, offsetsProm])
+        .then(([placesList, offsetsList]) => prog.emit('datafetched', [placesList, offsetsList]))
+        .catch(err => {// eslint-disable-next-line no-console
+          console.log('dynomodb error: ', err.message, err);
           res.write(`event: status\ndata: ${err.message}. Please try again.\n\n\n`);
-        }
-      }
+        });
+
+      // if (records.length == 0) {
+      //   throw new Error('Database error: Data not available');
+      // } else {
+      //   prog.emit('datafetched', [oList, pList]);
+      // }
+
+     
 
       req.on('close', async () => {
         try {
           clearInterval(eventInterval);
-          await client.close();
-          client = null;
-          db = null;
+          // await client.close();
+          // client = null;
+          // db = null;
         } catch (err) {// eslint-disable-next-line no-console
           console.log(err);
         } finally {
